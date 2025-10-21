d0f6b7f31757fd390c72c2e2ee4a359ef7c919d0
舒克 <1435787623@qq.com>
2025-10-21 19:59:31 +0800

    fix；修复若干个bugg

---
+++ next-sitemap.config.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: zlc
+ * @Date: 2025-10-21 19:48:14
+ * @LastEditTime: 2025-10-21 19:48:51
+ * @LastEditors: zlc
+ * @Description: 
+ * @FilePath: \cali.so\next-sitemap.config.js
+ */
+module.exports = {  
+  siteUrl: 'https://www.ps520.asia',  
+  generateRobotsTxt: true,  
+  changefreq: 'daily',  
+  priority: 0.7,  
+  sitemapSize: 5000,  
+};  
